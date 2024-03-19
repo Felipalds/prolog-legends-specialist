7ca51249186d7686397d25f89ba11ca9c67458d5
Pedro H. F. Zoz <107139787+Pfzoz@users.noreply.github.com>
2024-03-18 22:47:19 -0300

    Update prolog.pl

--- prolog.pl
+++ prolog.pl
@@ -27,13 +27,112 @@ get_class(Champion) :-
     assertz(champion_class(Champion, Class)),
     write("So the class is "), write(Class), nl.
 
+tank_matchup(OldList, NewList, UserType) :-
+    write('O inimigo builda vida ou resistência? (vida/resistencia) '), nl,
+    read(BuildType),
+    (UserType == mage, BuildType == vida ->
+        NewList = [liandry | OldList];
+        NewList = OldList),
+    (UserType == mage, BuildType == resistencia ->
+        NewList = [vazio | OldList];
+        NewList = OldList),
+    (UserType == adc, BuildType == vida ->
+        NewList = [rei_destruido | OldList];
+        NewList = OldList),
+    (UserType == adc, BuildType == resistencia ->
+        NewList = [dominik | OldList];
+        NewList = OldList),
+    (UserType == assassin, BuildType == resistencia ->
+        NewList = [serylda | OldList];
+        NewList = OldList).
+
+healing_matchup(OldList, NewList, UserType) :-
+    (UserType == mage ->
+        NewList = [morellonomicon | OldList];
+        NewList = OldList),
+    (UserType == adc ->
+        NewList = [lembrete_mortal | OldList];
+        NewList = OldList),
+    (UserType == bruiser ->
+        NewList = [quimiopunk | OldList];
+        NewList = OldList),
+    (UserType == tank ->
+        NewList = [espinhos | OldList];
+        NewList = OldList),
+    (UserType == assassin ->
+        NewList = [quimiopunk | OldList];
+        NewList = OldList).
+
+
+cc_matchup(OldList, NewList, UserType) :-
+    (UserType == mage ->
+        NewList = [banshee | OldList];
+        NewList = OldList),
+    (UserType == adc ->
+        NewList = [botas_mr | OldList];
+        NewList = OldList),
+    (UserType == bruiser ->
+        NewList = [forca_da_natureza | OldList];
+        NewList = OldList),
+    (UserType == tank ->
+        NewList = [forca_da_natureza | OldList];
+        NewList = OldList),
+    (UserType == assassin ->
+        NewList = [botas_mr | OldList];
+        NewList = OldList).
+
 add_item_on_list(OldList, NewList) :-
     champion_class(enemy, EnemyType),
     champion_class(user, UserType),
+    % ADC Match-ups
     (EnemyType == adc, UserType == mage ->
         NewList = [ludens | OldList];
+        NewList = OldList),
+    (EnemyType == adc, UserType == tank ->
+        NewList = [egide_de_fogo | OldList];
+        NewList = OldList),
+    (EnemyType == adc, UserType == assassin ->
+        NewList = [cicloespada | OldList];
+        NewList = OldList),
+    (EnemyType == adc, UserType == bruiser ->
+        NewList = [trindade | OldList];
+        NewList = OldList),
+    % Bruiser Match-ups
+    (EnemyType == bruiser, UserType == mage ->
+        NewList = [zhonyas | OldList];
+        NewList = OldList),
+    (EnemyType == bruiser, UserType == adc ->
+        NewList = [arco_escudo | OldList];
+        NewList = OldList),
+    (EnemyType == bruiser, UserType == assassin ->
+        NewList = [eclipse | OldList];
+        NewList = OldList),
+    % Assassin Match-ups
+    (EnemyType == assassin, UserType == mage ->
+        NewList = [zhonyas | OldList];
+        NewList = OldList),
+    (EnemyType == assassin, UserType == adc ->
+        NewList = [arco_escudo | OldList];
+        NewList = OldList),
+    (EnemyType == assassin, UserType == bruiser ->
+        NewList = [sterak | OldList];
+        NewList = OldList),
+    % Tank Match-ups
+    (EnemyType == tank ->
+        tank_matchup(OldList, NewList, UserType);
+        NewList = OldList),
+    % Healing Choice
+    write('O inimigo se cura? sim/nao')
+    read(EnemyHeals)
+    (EnemyHeals == 'sim' ->
+        healing_matchup(OldList, NewList, UserType);
+        NewList = OldList),
+    write('O inimigo tem CC (Crowd Control)? sim/nao')
+    read(EnemyHasCC)
+    (EnemyHasCC == 'sim' ->
+        cc_matchup(OldList, NewList, UserType);
         NewList = OldList).
-
+    
 
 save_list(List) :-
     retractall(saved_list(_)),
